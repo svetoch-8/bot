cbf020b3e29281710ddd88279f2332c183c37df5
svetoch-8 <65140909+svetoch-8@users.noreply.github.com>
2021-09-16 19:56:35 +0300

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODg4MDU5NzU4MTI3NTA1NDA4.YUNMGQ.wN6opI8l9KZZ0tb3Qa59RhoDhMM',
+        prefix: 'p',
+        activity: '',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
---
+++ s.bat
@@ -0,0 +1,3 @@
+@echo off
+node index.js
+npm start #Indicated in package.json
